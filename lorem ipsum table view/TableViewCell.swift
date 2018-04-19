d9b8b6300a22fa6638dc075e63ac719438cee9b9
Scott Chow <tehchowster@gmail.com>
2018-04-18 18:00:19 -0700

    Initial Commit

---
+++ lorem ipsum table view/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  lorem ipsum table view
+//
+//  Created by Scott P. Chow on 4/18/18.
+//  Copyright © 2018 something here. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    
+
+}
---
+++ lorem ipsum table view/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  lorem ipsum table view
+//
+//  Created by Scott P. Chow on 4/18/18.
+//  Copyright © 2018 something here. All rights reserved.
+//
+let strings = [ "Chapter too parties its letters nor. Cheerful but whatever ladyship disposed yet judgment. Lasted answer oppose to ye months no esteem. Branched is on an ecstatic directly it. Put off continue you denoting returned juvenile. Looked person sister result mr to. Replied demands charmed do viewing ye colonel to so. Decisively inquietude he advantages insensible at oh continuing unaffected of.", "Needed feebly dining oh talked wisdom oppose at. Applauded use attempted strangers now are middleton concluded had. It is tried ﻿no added purse shall no on truth. Pleased anxious or as in by viewing forbade minutes prevent. Too leave had those get being led weeks blind. Had men rose from down lady able. Its son him ferrars proceed six parlors. Her say projection age announcing decisively men. Few gay sir those green men timed downs widow chief. Prevailed remainder may propriety can and.", "Contented get distrusts certainty nay are frankness concealed ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects. End sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte sympathize was. He families believed if no elegance interest surprise an. It abode wrong miles an so delay plate. She relation own put outlived may disposed. ", "Hey there."]
+
+import UIKit
+
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        if indexPath.row % 2 == 0 {
+            cell.contentView.backgroundColor = UIColor.red
+        }
+        cell.label.text = strings[indexPath.row]
+        return cell
+    }
+}
+
